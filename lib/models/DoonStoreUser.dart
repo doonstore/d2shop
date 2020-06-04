f22f1007b9568b02e2b0c564eef1447b692a0f69
Lallu Anthoor <lalluanthoor@gmail.com>
2020-06-04 20:27:09 +0530

    fix: handle address array
    Signed-off-by: Lallu Anthoor <lalluanthoor@gmail.com>

--- lib/models/DoonStoreUser.dart
+++ lib/models/DoonStoreUser.dart
@@ -6,7 +6,7 @@ class DoonStoreUser {
   String displayName;
   String email;
   String phone;
-  List<Address> addressList;
+  List<Address> addressList = List();
   String photoUrl;
   DateTime lastLogin;
 
@@ -30,7 +30,7 @@ class DoonStoreUser {
     this.photoUrl = data['photoUrl'];
     this.phone = data['phone'];
     this.lastLogin = (data['lastLogin'] as Timestamp).toDate();
-    this.addressList = List.castFrom(data['addressList']);
+    setAddressList(data['addressList']);
   }
 
   Map<String, dynamic> toMap() {
@@ -41,8 +41,22 @@ class DoonStoreUser {
       'photoUrl': photoUrl,
       'phone': phone,
       'lastLogin': Timestamp.fromDate(lastLogin),
-      'addressList': addressList
+      'addressList': getAddressList()
     };
+  }
+
+  List<String> getAddressList() {
+    List<String> addressList = List();
+    for (Address address in this.addressList) {
+      addressList.add(address.toString());
+    }
+    return addressList;
+  }
+
+  void setAddressList(List<dynamic> addressList) {
+    addressList.cast<String>().forEach((element) {
+      this.addressList.add(Address.fromString(element));
+    });
   }
 }
 
@@ -55,4 +69,25 @@ class Address {
   String country;
   String pinCode;
   String contactNumber;
+
+  Address(this.lineOne, this.lineTwo, this.street, this.city, this.state,
+      this.country, this.pinCode, this.contactNumber);
+
+  Address.fromString(String addressString) {
+    List<String> parts = addressString.split("##");
+    this.lineOne = parts[0].trim().length == 0 ? null : parts[0].trim();
+    this.lineTwo = parts[1].trim().length == 0 ? null : parts[1].trim();
+    this.street = parts[2].trim().length == 0 ? null : parts[2].trim();
+    this.city = parts[3].trim().length == 0 ? null : parts[3].trim();
+    this.state = parts[4].trim().length == 0 ? null : parts[4].trim();
+    this.country = parts[5].trim().length == 0 ? null : parts[5].trim();
+    this.pinCode = parts[6].trim().length == 0 ? null : parts[6].trim();
+    this.contactNumber = parts[7].trim().length == 0 ? null : parts[7].trim();
+  }
+
+  @override
+  String toString() {
+    return "$lineOne##$lineTwo##$street##$city##$state##$country##$pinCode##"
+        "$contactNumber";
+  }
 }
